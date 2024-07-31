8c078b12f712eb6d87e5733a3b53c399159bbbfe
unknown <Github@08>
2024-07-31 14:50:35 +0530

    Intial Commit

---
+++ todo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    completed =models.BooleanField(default = False)
+
+# Create your models here.
